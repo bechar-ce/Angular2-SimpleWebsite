b31b0ebe417033fd3bfc0097dc861b4414501fad
ccetkemche <bechar_ce@rediffmail.com>
2017-04-27 18:35:31 +0530

    - Data change

--- src/app/app-about/app-about.component.ts
+++ src/app/app-about/app-about.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   templateUrl: './app-about.component.html',
   styleUrls: ['./app-about.component.css']
 })
 export class AppAboutComponent implements OnInit {
+  public title: string = 'About Us';
+  public pageTitleDescription: string = "Why you love to learn AngularJS with us";
+  public pageDescription: string = "Hi there! thanks for dropping by <br> <br> I have a passion for creating value in the form of functional, reliable software and websites. My current focus is AngularJS. Earlier in my career, I worked with Flash, Flex, ActionScript 2.0\/3.0 and other Adobe technologies.<br> <br>";
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
